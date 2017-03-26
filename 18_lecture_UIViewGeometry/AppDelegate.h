fd396fb11534ef8f7490fc987fb537894af969dc
Slava <rtootik@gmail.com>
2017-03-26 13:43:58 +0300

    Initial Commit

---
+++ 18_lecture_UIViewGeometry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  18_lecture_UIViewGeometry
+//
+//  Created by Slava on 26.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 18_lecture_UIViewGeometry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  18_lecture_UIViewGeometry
+//
+//  Created by Slava on 26.03.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
